d7effa70b7252c370083b03d41f2283b50d6de4c
MontolioV <montoliov@gmail.com>
2019-02-09 15:27:11 +0200

    PROD_URL nuxt environment variables dependency for auth0 implementation.

--- assets/js/auth0Impl.js
+++ assets/js/auth0Impl.js
@@ -4,7 +4,9 @@ class Auth0Impl {
   webAuth = new auth0.WebAuth({
     domain: 'timeman.auth0.com',
     clientID: 'PsEsUB705UNm8ovl3BqD8ZbeQpNn13Nu',
-    returnTo: 'http://localhost:3000/auth/cb',
+    returnTo: process.env.PROD_URL
+      ? process.env.PROD_URL + '/auth/cb'
+      : 'http://localhost:3000/auth/cb',
     responseType: 'token',
     audience: 'https://montoliov.ml/rs',
     scope: 'openid profile email',
