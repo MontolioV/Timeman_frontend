2185e033a652e3c8e5bfa1bdf33129d4fc992e97
MontolioV <montoliov@gmail.com>
2019-02-11 15:35:47 +0200

    Auth0Impl unnecessary returnTo param removed.

--- assets/js/auth0Impl.js
+++ assets/js/auth0Impl.js
@@ -4,9 +4,6 @@ class Auth0Impl {
   webAuth = new auth0.WebAuth({
     domain: 'timeman.auth0.com',
     clientID: 'PsEsUB705UNm8ovl3BqD8ZbeQpNn13Nu',
-    returnTo: process.env.PROD_URL
-      ? process.env.PROD_URL + '/auth/cb'
-      : 'http://localhost:3000/auth/cb',
     responseType: 'token',
     audience: 'https://montoliov.ml/rs',
     scope: 'openid profile email',
